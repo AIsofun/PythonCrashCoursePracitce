eeb769ed4a6c56cf190b0e587452cb3d9dcf6364
AIsofun <wethredic@gmail.com>
2025-02-26 15:33:22 +0800

    Initial commit

---
+++ basic/chapter-all.py
@@ -0,0 +1,11 @@
+#打印
+print("Hello, world!")
+
+message = "Hello Python world!"
+print(message)
+message = "Hello Python Crash Course world!"
+print(message)
+
+#字符串
+"This is a string."
+'This is also a string.'
